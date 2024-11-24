f7e34f0c1fbde2c20fa4f3331c2c3b74db3ccde5
Gold87 <91761103+Gold872@users.noreply.github.com>
2024-11-24 11:26:48 -0500

    Added network detector

---
+++ lib/src/simulator/network_detector.dart
@@ -0,0 +1,47 @@
+﻿import "package:autonomy/autonomy.dart";
+import "package:burt_network/burt_network.dart";
+
+class NetworkDetector extends DetectorInterface {
+  final List<GpsCoordinates> _newObstacles = [];
+  bool _hasNewObstacles = false;
+
+  NetworkDetector({required super.collection});
+
+  @override
+  bool canSeeAruco() => false;
+
+  @override
+  Future<void> dispose() async {}
+
+  @override
+  bool findObstacles() {
+    for (final obstacle in _newObstacles) {
+      collection.pathfinder.recordObstacle(obstacle);
+    }
+    final newObstacles = _hasNewObstacles;
+    _hasNewObstacles = false;
+
+    _newObstacles.clear();
+
+    return newObstacles;
+  }
+
+  @override
+  Future<bool> init() async {
+    collection.server.messages.onMessage(
+        name: AutonomyData().messageName,
+        constructor: AutonomyData.fromBuffer,
+        callback: _onDataReceived);
+    return true;
+  }
+
+  void _onDataReceived(AutonomyData data) {
+    if (data.obstacles.isNotEmpty) {
+      _newObstacles.addAll(data.obstacles);
+      _hasNewObstacles = true;
+    }
+  }
+
+  @override
+  bool isOnSlope() => false;
+}
