d55748ecf923ff134d0a6605199d2ffee5140bf4
Gold87 <91761103+Gold872@users.noreply.github.com>
2025-02-03 16:44:35 -0500

    Use meters for goForward()

--- lib/src/utils/gps_utils.dart
+++ lib/src/utils/gps_utils.dart
@@ -3,6 +3,24 @@ import "dart:math";
 
 import "package:autonomy/interfaces.dart";
 
+/// An alias for gps coordinates measured in meters
+typedef GpsMeters = ({num lat, num long});
+
+/// Utility math methods for GpsMeters
+extension GpsMetersUtil on GpsMeters {
+  /// Add 2 [GpsMeters] together
+  GpsMeters operator +(GpsMeters other) => (
+        lat: lat + other.lat,
+        long: long + other.long,
+      );
+
+  /// Subtract 2 [GpsMeters] from each other
+  GpsMeters operator -(GpsMeters other) => (
+        lat: lat - other.lat,
+        long: long - other.long,
+      );
+}
+
 extension GpsUtils on GpsCoordinates {
   static double maxErrorMeters = 0.5;
   static double moveLengthMeters = 1;
@@ -21,6 +39,15 @@ extension GpsUtils on GpsCoordinates {
   static GpsCoordinates get southEast => south + east;
   static GpsCoordinates get southWest => south + west;
 
+  static GpsMeters get eastMeters => (lat: 0, long: moveLengthMeters);
+  static GpsMeters get westMeters => (lat: 0, long: -moveLengthMeters);
+  static GpsMeters get northMeters => (lat: moveLengthMeters, long: 0);
+  static GpsMeters get southMeters => (lat: -moveLengthMeters, long: 0);
+  static GpsMeters get northEastMeters => northMeters + eastMeters;
+  static GpsMeters get northWestMeters => northMeters + westMeters;
+  static GpsMeters get southEastMeters => southMeters + eastMeters;
+  static GpsMeters get southWestMeters => southMeters + westMeters;
+
   // Taken from https://stackoverflow.com/a/39540339/9392211
   static const metersPerLatitude = 111.32 * 1000;  // 111.32 km
   static const radiansPerDegree = pi / 180;
@@ -36,7 +63,7 @@ extension GpsUtils on GpsCoordinates {
 
   double heuristicDistance(GpsCoordinates other) {
     var distance = 0.0;
-    final delta = (this - other).inMeters;
+    final delta = inMeters - other.inMeters;
     final deltaLat = delta.lat.abs();
     final deltaLong = delta.long.abs();
 
@@ -54,21 +81,19 @@ extension GpsUtils on GpsCoordinates {
     return distance;
   }
 
-  double manhattanDistance(GpsCoordinates other) =>
-    (latitude - other.latitude).abs() * metersPerLatitude +
-    (longitude - other.longitude).abs() * metersPerLongitude;
+  double manhattanDistance(GpsCoordinates other) {
+    final delta = inMeters - other.inMeters;
+    return delta.lat.toDouble().abs() + delta.long.abs();
+  }
 
   bool isNear(GpsCoordinates other, [double? tolerance]) {
     tolerance ??= maxErrorMeters;
     final currentMeters = inMeters;
     final otherMeters = other.inMeters;
 
-    final (deltaX, deltaY) = (
-      currentMeters.lat - otherMeters.lat,
-      currentMeters.long - otherMeters.long
-    );
+    final delta = currentMeters - otherMeters;
 
-    final distance = sqrt(pow(deltaX, 2) + pow(deltaY, 2));
+    final distance = sqrt(pow(delta.long, 2) + pow(delta.lat, 2));
 
     return distance < tolerance;
   }
@@ -85,14 +110,27 @@ extension GpsUtils on GpsCoordinates {
 
   String prettyPrint() => toProto3Json().toString();
 
-  GpsCoordinates goForward(CardinalDirection orientation) => this + switch(orientation) {
-    CardinalDirection.north => GpsUtils.north,
-    CardinalDirection.south => GpsUtils.south,
-    CardinalDirection.west => GpsUtils.west,
-    CardinalDirection.east => GpsUtils.east,
-    CardinalDirection.northEast => GpsUtils.northEast,
-    CardinalDirection.northWest => GpsUtils.northWest,
-    CardinalDirection.southEast => GpsUtils.southEast,
-    CardinalDirection.southWest => GpsUtils.southWest,
-  };
+  // GpsCoordinates goForward(CardinalDirection orientation) => this +
+  //     switch (orientation) {
+  //       CardinalDirection.north => GpsUtils.north,
+  //       CardinalDirection.south => GpsUtils.south,
+  //       CardinalDirection.west => GpsUtils.west,
+  //       CardinalDirection.east => GpsUtils.east,
+  //       CardinalDirection.northEast => GpsUtils.northEast,
+  //       CardinalDirection.northWest => GpsUtils.northWest,
+  //       CardinalDirection.southEast => GpsUtils.southEast,
+  //       CardinalDirection.southWest => GpsUtils.southWest,
+  //     };
+
+  GpsCoordinates goForward(CardinalDirection orientation) => (inMeters +
+    switch (orientation) {
+      CardinalDirection.north => GpsUtils.northMeters,
+      CardinalDirection.south => GpsUtils.southMeters,
+      CardinalDirection.west => GpsUtils.westMeters,
+      CardinalDirection.east => GpsUtils.eastMeters,
+      CardinalDirection.northEast => GpsUtils.northEastMeters,
+      CardinalDirection.northWest => GpsUtils.northWestMeters,
+      CardinalDirection.southEast => GpsUtils.southEastMeters,
+      CardinalDirection.southWest => GpsUtils.southWestMeters,
+    }).toGps();
 }
