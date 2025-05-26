cd2ab752769bcb5df2e21fe6fe6eca9b0c1594d9
Gold87 <91761103+Gold872@users.noreply.github.com>
2025-05-26 13:26:32 -0400

    Repaired and removed old legacy tests

--- test/rover_test.dart
+++ test/rover_test.dart
@@ -4,7 +4,6 @@ import "package:burt_network/logging.dart";
 
 import "package:autonomy/interfaces.dart";
 import "package:autonomy/rover.dart";
-import "package:autonomy/simulator.dart";
 
 void main() => group("[Rover]", tags: ["rover"], () {
   test("Can be restarted", () async {
@@ -13,17 +12,6 @@ void main() => group("[Rover]", tags: ["rover"], () {
     await rover.init();
     await rover.restart();
     await rover.dispose();
-  });
-
-  test("Real pathfinding is coherent", () async {
-    Logger.level = LogLevel.off;
-    final simulator = AutonomySimulator();
-    simulator.pathfinder = RoverPathfinder(collection: simulator);
-    await testPath(simulator);
-    simulator.gps.update((lat: 0, long: 0).toGps());
-    simulator.imu.update(Orientation());
-    await testPath2(simulator);
-    await simulator.dispose();
   });
 
   test("Waits for sensor data", () async {
@@ -55,45 +43,3 @@ void main() => group("[Rover]", tags: ["rover"], () {
     await rover.dispose();
   });
 });
-
-Future<void> testPath(AutonomyInterface simulator) async {
-  final destination = (lat: 5, long: 5).toGps();
-  final result = simulator.pathfinder.getPath(destination);
-  expect(simulator.gps.latitude, 0);
-  expect(simulator.gps.longitude, 0);
-  expect(simulator.imu.heading, 0);
-  expect(result, isNotNull); if (result == null) return;
-  expect(result, isNotEmpty);
-  for (final transition in result) {
-    simulator.logger.trace("  From: ${simulator.gps.coordinates.prettyPrint()} facing ${simulator.imu.heading}");
-    simulator.logger.debug("  $transition");
-    await simulator.drive.driveState(transition);
-    expect(simulator.gps.isNear(transition.position), isTrue);
-    simulator.logger.trace("New orientation: ${simulator.imu.heading}");
-    simulator.logger.trace("Expected orientation: ${transition.orientation}");
-    expect(simulator.imu.nearest, transition.orientation);
-  }
-}
-
-Future<void> testPath2(AutonomyInterface simulator) async {
-  // Logger.level = LogLevel.all;
-  final destination = (lat: 4, long: 0).toGps();
-  simulator.pathfinder.recordObstacle((lat: 2, long: 0).toGps());
-  simulator.pathfinder.recordObstacle((lat: 4, long: -1).toGps());
-  simulator.pathfinder.recordObstacle((lat: 4, long: 1).toGps());
-  final result = simulator.pathfinder.getPath(destination);
-  expect(simulator.gps.latitude, 0);
-  expect(simulator.gps.longitude, 0);
-  expect(simulator.imu.heading, 0);
-  expect(result, isNotNull); if (result == null) return;
-  expect(result, isNotEmpty);
-  for (final transition in result) {
-    simulator.logger.debug(transition.toString());
-    simulator.logger.trace("  From: ${simulator.gps.coordinates.prettyPrint()}");
-    await simulator.drive.driveState(transition);
-    expect(simulator.gps.isNear(transition.position), isTrue);
-    expect(simulator.pathfinder.isObstacle(simulator.gps.coordinates), isFalse);
-    expect(simulator.imu.nearest, transition.orientation);
-    simulator.logger.trace("  To: ${simulator.gps.coordinates.prettyPrint()}");
-  }
-}
